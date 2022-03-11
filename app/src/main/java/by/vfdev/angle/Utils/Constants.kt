fb100750ec3e8406ccf9866848d5d9770257f37d
VasiliFralou <Dr1fteR20021998>
2022-03-11 16:20:11 +0300

    0.0.23 (23)

--- app/src/main/java/by/vfdev/angle/Utils/Constants.kt
+++ app/src/main/java/by/vfdev/angle/Utils/Constants.kt
@@ -3,6 +3,6 @@ package by.vfdev.angle.Utils
 class Constants {
 
     companion object {
-        const val BASE_URL = "https://raw.githubusercontent.com/VasiliFralou/Angle/master/app/src/main/java/by/vfdev/angle/JSON/"
+        const val BASE_URL = "https://raw.githubusercontent.com/VasiliFralou/AngleJSON/main/"
     }
 }
